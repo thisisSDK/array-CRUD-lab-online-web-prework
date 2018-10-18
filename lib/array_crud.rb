7c33c06487b5342a5f0fba32202fd268f136d69c
sdk1270 <sdk1270@gmail.com>
2018-10-18 16:29:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_to = ["I", "am", "really", "learning"]
-  element << "arrays!"
+  add_to << "element"
 end
 
 def add_element_to_start_of_array(array, element)
