340770e2c9ddd1932f230c416d86ca3a91976f5b
sdk1270 <sdk1270@gmail.com>
2018-10-18 16:12:06 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_to = ["a", "b", "c"]
+  add_to << "d"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = ["a", "b", "c"]
+    array.push("element")
 end
 
 def remove_element_from_end_of_array(array)
