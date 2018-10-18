98e6115718016752c61aff5456ce3701d1fe9f0b
sdk1270 <sdk1270@gmail.com>
2018-10-18 16:25:55 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-      array = ["wow", "am", "c", "arrays!"]
-      array = [0]
+      array = ["wow", "I", "am", "really", "learning", "arrays!"]
+      index_number = [2]
 
 end
 
 def retrieve_first_element_from_array(array)
-      array = ["wow", "b", "c", "d"]
+      array = ["wow", "I", "am", "really", "learning", "arrays!"]
       array = [0]
 end
 
